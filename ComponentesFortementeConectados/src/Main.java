b24e84cb0d367d23964bb23a82cb7aac5ac5182c
Jhone Mendonça <g.m.enohj@gmail.com>
2016-09-15 18:02:44 -0400

    grafo base
    leitura da matriz de adjacências
    esqueleto da função de busca

--- ComponentesFortementeConectados/src/Main.java
+++ ComponentesFortementeConectados/src/Main.java
@@ -1,8 +1,63 @@
-public class Main {
 
+import  java.util.Scanner;
+class Main
+{
+    private static Scanner entrada = new Scanner(System.in);
     
-    public static void main(String[] args) {
-        
+    public static void main(String[] args)
+    {
+        int ordem, i, j;
+        Grafo g;
+        // leitura da ordem da matriz
+        ordem = entrada.nextInt();
+        g = new Grafo(ordem);
+        // leitura da matriz de adjacencias
+        for(i = 1 ; i <= ordem ; i++)
+        {
+            for(j = 1 ; j <= ordem ; j++)
+            {
+                g.AddAresta(i, j, entrada.nextInt());
+            }
+        }
+        System.out.println("\n\n" + g.GetComponentesFortementeConectados());
+        //g.ImprimirAdjacencias();
     }
-    
+    private static class Grafo
+    {
+        private int adj[][];
+        private int vertices = 0;
+
+        public Grafo(int quantidade)
+        {
+            vertices = quantidade;
+            adj = new int [quantidade + 1][quantidade + 1];
+        }
+        public int getAresta(int origem, int destino)
+        {
+            return adj[origem][destino];
+        }
+        public void AddAresta(int linha, int coluna, int value)
+        {
+            adj[linha][coluna] = value;
+        }
+        public void ImprimirAdjacencias()
+        {
+            System.out.print("\n<!------------------------------------------------------->\n");
+            int i, j;
+            for(i = 1 ; i <= vertices ; i++)
+            {
+                for(j = 1 ; j <= vertices ; j++)
+                {
+                    System.out.print(adj[i][j] + " ");
+                }
+                System.out.println("");
+            }
+        }
+        public int GetComponentesFortementeConectados()
+        {
+            
+            return 0;
+        }
+    }
 }
+ 
